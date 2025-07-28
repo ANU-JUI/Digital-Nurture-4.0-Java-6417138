7d3e50dfa2aada96c29b73eb3575d7758cca13cd
Anushka Dutta <anushka.barasat@gmail.com>
2025-07-28 11:59:28 +0530

    12-React-Hol week7 commited

---
+++ week7/12-React-Hol/code/ticketbookingapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginControl from './components/LoginControl';
+
+function App() {
+  return (
+     <div className="App">
+      <header >
+        <LoginControl />
+      </header>
+    </div>
+  );
+}
+
+export default App;
